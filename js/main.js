e4b5b82d7dd8c7fad326f20f0fae0dfa1ff6a557
Federico <federicobartoli@me.com>
2020-02-19 21:24:41 +0100

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Creo una lista di email
 // Chiedo all'utente di inserire la sua email
 // Se è nella lista rispondi che puoi accedere
-// Se non scrivi che non può accedere.
+// Se non , scrivi che non può accedere.
 // Far capire al programma cosa è una mail e cosa non
 
-var iscritti = ['luigi@email.com','luigi2@email.com','luigi3@email.com','luigi4@email.com','luigi5@email.com','luigi6@email.com','luigi7@email.com','luigi8@email.com','luigi9@email.com','luigi10@email.com','luigi11@email.com',];
+var iscritti = ['luigi@email.com','luigi2@email.com','luigi3@email.com','luigi4@email.com','luigi5@email.com','luigi6@email.com','luigi7@email.com','luigi8@email.com','luigi9@email.com','luigi10@email.com','luigi11@email.com'];
 var tuaEmail = prompt('Inserisci la tua email per poter accedere');
   console.log(tuaEmail);
+
+
+
 
 //Possiamo creare una variabile che controlli in che situazione ci troviamo
 var trovato = false;
@@ -24,9 +27,32 @@ for(var i = 0; i < iscritti.length; i++) {
 }
 
 if(trovato) {
-    document.getElementById('entra-alla-festa').innerHTML = 'Puoi entrare alla festa';
+  document.getElementById('entra-alla-festa').innerHTML = 'Puoi entrare alla festa';
 
-} else {
+}else {
   document.getElementById('non-entra-alla-festa').innerHTML = 'Non puoi entrare alla festa';
+
+}
+
+
+
+// inizio esercizio dadi
+
+// Creazione numeri random da 1 a 6
+// Creazione numeri random da 1 a 6
+// Confronto quale numero è più alto
+// Quello più alto vince
+
+var dado1 = Math.floor(Math.random() * 6) + 1;
+document.getElementById('dado-uno').innerHTML = dado1;
+var dado2 = Math.floor(Math.random() * 6) + 1;
+document.getElementById('dado-due').innerHTML = dado2;
+
+if (dado1 > dado2) {
+  document.getElementById('vincita-uno').innerHTML = 'Vince il dado uno';
+
+}
+else {
+  document.getElementById('vincita-due').innerHTML = 'Vince il dado due';
 
 }
