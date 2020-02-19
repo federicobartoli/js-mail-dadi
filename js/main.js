376c36dbfe56be77112d6ece6f98ebaa3e5657ce
Federico <federicobartoli@me.com>
2020-02-19 15:45:17 +0100

    Inizio Javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Creo una lista di email
+// Chiedo all'utente di inserire la sua email
+// Se è nella lista rispondi che puoi accedere
+// Se non scrivi che non può accedere.
+// Far capire al programma cosa è una mail e cosa non
+
+var iscritti = ['luigi@email.com','luigi2@email.com','luigi3@email.com','luigi4@email.com','luigi5@email.com','luigi6@email.com','luigi7@email.com','luigi8@email.com','luigi9@email.com','luigi10@email.com','luigi11@email.com',];
+var tuaEmail = prompt('Inserisci la tua email per poter accedere');
+  console.log(tuaEmail);
+if (tuaEmail == iscritti) {
+  console.log('ciao');
+}
